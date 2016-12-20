46eb13d14328c79cdd4345a5c715140636e799b8
Gerry Mc Donnell <irishbloke@gmail.com>
2016-12-20 07:23:25 +0000

    Update dbtables.sql

--- dbtables.sql
+++ dbtables.sql
@@ -1,23 +1,5 @@
--- phpMyAdmin SQL Dump
--- version 4.5.1
--- http://www.phpmyadmin.net
 --
--- Host: 127.0.0.1
--- Generation Time: Jun 11, 2016 at 09:18 PM
--- Server version: 10.1.10-MariaDB
--- PHP Version: 5.6.19
-
-SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
-SET time_zone = "+00:00";
-
-
-/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
-/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
-/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
-/*!40101 SET NAMES utf8mb4 */;
-
---
--- Database: `cakemp3`
+-- Database: `cakenewsapp`
 --
 
 -- --------------------------------------------------------
@@ -29,7 +11,7 @@ SET time_zone = "+00:00";
 CREATE TABLE `changelogs` (
   `id` int(11) NOT NULL,
   `title` varchar(255) NOT NULL,
-  `changelogscategories_id` int(11) NOT NULL,
+  `changelog_category_id` int(11) NOT NULL,
   `description` text NOT NULL,
   `priority` int(11) NOT NULL,
   `url` varchar(255) NOT NULL,
@@ -39,13 +21,12 @@ CREATE TABLE `changelogs` (
   `modified` datetime NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=latin1;
 
--- --------------------------------------------------------
 
 --
--- Table structure for table `changelogs_categories`
+-- Table structure for table `changelog_categories`
 --
 
-CREATE TABLE `changelogs_categories` (
+CREATE TABLE `changelog_categories` (
   `id` int(11) NOT NULL,
   `title` varchar(50) NOT NULL,
   `created` datetime NOT NULL,
@@ -53,17 +34,33 @@ CREATE TABLE `changelogs_categories` (
 ) ENGINE=InnoDB DEFAULT CHARSET=latin1;
 
 --
--- Dumping data for table `changelogs_categories`
+-- Dumping data for table `changelog_categories`
 --
 
-INSERT INTO `changelogs_categories` (`id`, `title`, `created`, `modified`) VALUES
-(0, 'Changelog', '2016-04-11 17:13:46', '2016-04-11 17:13:46'),
-(1, 'Idea', '2016-04-10 21:11:29', '2016-04-10 21:11:29'),
+INSERT INTO `changelog_categories` (`id`, `title`, `created`, `modified`) VALUES
+(1, 'Changelog', '2016-04-11 17:13:46', '2016-04-11 17:13:46'),
 (2, 'Bug', '2016-04-10 21:11:35', '2016-04-10 21:11:35'),
 (3, 'To Do', '2016-04-10 21:11:41', '2016-04-10 21:11:41'),
 (4, 'Article', '2016-04-15 17:52:34', '2016-05-04 13:14:41'),
 (5, 'Plugins', '2016-05-07 20:48:47', '2016-06-04 20:36:29'),
-(6, 'Project', '2016-06-11 10:34:56', '2016-06-11 10:34:56');
+(6, 'Project', '2016-06-11 10:34:56', '2016-06-11 10:34:56'),
+(8, 'Idea', '2016-04-10 21:11:29', '2016-04-10 21:11:29');
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `users`
+--
+
+CREATE TABLE `users` (
+  `id` int(11) NOT NULL,
+  `username` varchar(255) NOT NULL,
+  `password` varchar(255) NOT NULL,
+  `role` varchar(10) NOT NULL DEFAULT 'user',
+  `created` datetime NOT NULL,
+  `modified` datetime NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
 
 --
 -- Indexes for dumped tables
@@ -76,11 +73,17 @@ ALTER TABLE `changelogs`
   ADD PRIMARY KEY (`id`);
 
 --
--- Indexes for table `changelogs_categories`
+-- Indexes for table `changelog_categories`
 --
-ALTER TABLE `changelogs_categories`
+ALTER TABLE `changelog_categories`
+  ADD PRIMARY KEY (`id`);
+
+--
+-- Indexes for table `users`
+--
+ALTER TABLE `users`
   ADD PRIMARY KEY (`id`),
-  ADD UNIQUE KEY `title` (`title`);
+  ADD UNIQUE KEY `username` (`username`);
 
 --
 -- AUTO_INCREMENT for dumped tables
@@ -90,7 +93,14 @@ ALTER TABLE `changelogs_categories`
 -- AUTO_INCREMENT for table `changelogs`
 --
 ALTER TABLE `changelogs`
-  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=3;
-/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
-/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
-/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=11;
+--
+-- AUTO_INCREMENT for table `changelog_categories`
+--
+ALTER TABLE `changelog_categories`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=10;
+--
+-- AUTO_INCREMENT for table `users`
+--
+ALTER TABLE `users`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=2;
